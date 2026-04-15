a806a1e5b226daf5bb784491f5d6548d201d836b
Tomasz Tarnogrodzki <tarnogrodzki@google.com>
2026-04-15 08:13:42 +0000

    Fix method handler cardinality check deadlocks

--- include/grpcpp/support/method_handler.h
+++ include/grpcpp/support/method_handler.h
@@ -24,6 +24,7 @@
 #include <grpcpp/support/byte_buffer.h>
 #include <grpcpp/support/sync_stream.h>
 
+#include <thread>
 #include "absl/log/absl_check.h"
 
 namespace grpc {
@@ -55,14 +56,31 @@ inline void CheckForExtraRequests(grpc::internal::Call* call,
                                   grpc::Status* status,
                                   const char* error_message) {
   if (!status->ok()) return;
-  grpc::internal::CallOpSet<grpc::internal::CallOpGenericRecvMessage> check_ops;
-  grpc::ByteBuffer extra_msg;
-  check_ops.RecvMessage(&extra_msg);
-  check_ops.AllowNoMessage();
-  check_ops.FillOps(call);
-  call->cq()->Pluck(&check_ops);
-  if (check_ops.got_message) {
-    *status = grpc::Status(grpc::StatusCode::UNIMPLEMENTED, error_message);
+  grpc_byte_buffer** extra_msg = new grpc_byte_buffer*(nullptr);
+  grpc_op op;
+  op.op = GRPC_OP_RECV_MESSAGE;
+  op.flags = 0;
+  op.reserved = nullptr;
+  op.data.recv_message.recv_message = extra_msg;
+  if (grpc_call_start_batch(call->call(), &op, 1, extra_msg, nullptr) != GRPC_CALL_OK) {
+    delete extra_msg;
+    return;
+  }
+  gpr_timespec deadline = gpr_time_0(GPR_CLOCK_REALTIME);
+  grpc_event ev = grpc_completion_queue_pluck(call->cq()->cq(), extra_msg, deadline, nullptr);
+  if (ev.type != GRPC_QUEUE_TIMEOUT) {
+    if (*extra_msg != nullptr) {
+      *status = grpc::Status(grpc::StatusCode::UNIMPLEMENTED, error_message);
+      grpc_byte_buffer_destroy(*extra_msg);
+    }
+    delete extra_msg;
+  } else {
+    auto* cq = call->cq();
+    std::thread([cq, extra_msg]() {
+      grpc_completion_queue_pluck(cq->cq(), extra_msg, gpr_inf_future(GPR_CLOCK_REALTIME), nullptr);
+      if (*extra_msg != nullptr) grpc_byte_buffer_destroy(*extra_msg);
+      delete extra_msg;
+    }).detach();
   }
 }
 
@@ -229,10 +247,8 @@ class ServerStreamingHandler : public grpc::internal::MethodHandler {
                             "Cardinality violation: Server-streaming method "
                             "received extra request");
     }
-
     grpc::internal::CallOpSet<grpc::internal::CallOpSendInitialMetadata,
-                              grpc::internal::CallOpServerSendStatus>
-        ops;
+                              grpc::internal::CallOpServerSendStatus> ops;
     if (!param.server_context->sent_initial_metadata_) {
       ops.SendInitialMetadata(&param.server_context->initial_metadata_,
                               param.server_context->initial_metadata_flags());
